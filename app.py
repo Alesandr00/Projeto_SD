e2d00453af68466cc8d5b1a2baf0d6470b759744
Alesandro <105898363+Alesandr0@users.noreply.github.com>
2024-04-03 19:54:08 -0300

    Atualização app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__, static_folder='static')
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 video_time = 0
+usuarios = {}
 
 @app.route('/')
 def index():
@@ -14,7 +15,7 @@ def index():
 @app.route('/video')
 def video():
     try:
-        caminho_video = 'static/video.mp4'
+        caminho_video = os.path.join(app.static_folder, 'video.mp4')
         if not os.path.exists(caminho_video):
             abort(404)
         return send_file(caminho_video, mimetype='video/mp4')
@@ -25,20 +26,22 @@ def video():
 @socketio.on('connect')
 def handle_connect():
     print('Client connected: ' + request.sid)
-    emit('play', {'time': video_time}, broadcast=True)
 
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected: ' + request.sid)
+    if request.sid in usuarios:
+        del usuarios[request.sid]
 
 @socketio.on('time update')
 def handle_time_update(data):
     global video_time
     if 'time' in data and isinstance(data['time'], (int, float)):
         video_time = data['time']
+        emit('time update', {'time': video_time}, broadcast=True, include_self=False)
     else:
         print('Dados inválidos recebidos para atualização de tempo')
-        
+
 @socketio.on('message')
 def handle_message(data):
     print('Received message:', data)
@@ -47,7 +50,9 @@ def handle_message(data):
 @socketio.on('username')
 def handle_username(data):
     print('Received username:', data['username'])
-    emit('message', {'username': 'System', 'message': f'{data["username"]} - entrou no chat'}, broadcast=True)
+    usuarios[request.sid] = data['username']
+    emit('message', {'username': 'System', 'message': f'{data["username"]} entrou no chat'}, broadcast=True)
+    emit('message', {'username': 'System', 'message': f'Usuários online: {", ".join(usuarios.values())}'}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, port=3000)
+    socketio.run(app, host='0.0.0.0', port=3000)
